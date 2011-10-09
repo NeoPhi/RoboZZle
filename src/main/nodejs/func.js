3145b4aff7ca39d6e00424cfa3c6e623abb23546
Daniel Rinehart <danielr@neophi.com>
2011-10-09 12:51:38 -0400

    Green function test.

--- src/main/nodejs/func.js
+++ src/main/nodejs/func.js
@@ -1,5 +1,16 @@
 module.exports = function() {
     var that = {};
 
+    var commands = [];
+
+    that.addCommand = function(command) {
+        commands.push(command);
+        return that;
+    };
+
+    that.getCommand = function(index) {
+        return commands[index];
+    };
+
     return that;
 };
