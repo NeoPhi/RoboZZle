ad42e828dc3e496f9118f2617c910427642dc450
Daniel Rinehart <danielr@neophi.com>
2011-10-09 12:59:59 -0400

    Green program test.

--- src/main/nodejs/program.js
+++ src/main/nodejs/program.js
@@ -1,5 +1,14 @@
 module.exports = (function() {
     var that = {};
 
+    var func = require("func");
+
+    var functions = [];
+    functions[0] = func();
+
+    that.getFunction = function(index) {
+        return functions[index];
+    };
+
     return that;
 }());
