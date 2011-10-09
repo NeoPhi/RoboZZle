a55037ae29a754cc6e97de7369a5a7613ca2eb0d
Daniel Rinehart <danielr@neophi.com>
2011-10-09 13:03:56 -0400

    Red can step test

--- src/test/nodejs/program.spec.js
+++ src/test/nodejs/program.spec.js
@@ -8,8 +8,14 @@ describe("Program", function() {
         program = require("program");
     });
 
-    it("has default function", function() {
+    it("has functions", function() {
         var f1 = func();
         expect(program.addFunction(f1).getFunction(0)).toEqual(f1);
+    });
+
+    it("can step", function() {
+        var f1 = func();
+        f1.addCommand(command.F);
+        expect(program.nextStep()).toEqual(command.F);
     });
 });
