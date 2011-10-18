214a6b417069dc36d9529e7b33bc47e4f326e9b2
Daniel Rinehart <danielr@neophi.com>
2011-10-17 22:16:26 -0400

    Green star tests.

--- src/main/nodejs/star.js
+++ src/main/nodejs/star.js
@@ -1,2 +1,16 @@
 module.exports.create = function() {
+    var that = {};
+
+    var visited = false;
+
+    that.isVisited = function() {
+        return visited;
+    };
+
+    that.visit = function() {
+        visited = true;
+        return that;
+    };
+
+    return that;
 };
